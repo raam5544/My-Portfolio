83f7db14bf1ff498957f4554f3998a4c0007bf1c
raam5544 <jeya.colombo@gmail.com>
2025-01-17 18:58:44 +0530

    edited honey comp

--- src/Components/Honeycomp.js
+++ src/Components/Honeycomp.js
@@ -5,7 +5,7 @@ function Honeycomp() {
     const rows = 3; // Number of rows in the honeycomb grid
     const cols = 3; // Number of columns in the honeycomb grid
     let count = 0;
-    const images = [{ pic: './about.jpg', txt: 'About' }, { pic: './notepad.png', txt: 'note' }, { pic: './1663653933402.jpg', txt: '' }, { pic: '/projects.png', txt: 'project' }, { pic: './home.png', txt: 'Home' }, { pic: './home.png', txt: 'Home' }, { pic: './home.png', txt: 'Home' }]
+    const images = [{ pic: './about.jpg', txt: 'About Me' }, { pic: './notepad.png', txt: 'Send Query' }, { pic: './1663653933402.jpg', txt: '' }, { pic: '/projects.png', txt: 'Project' }, { pic: './contact.png', txt: 'Contacts' }, { pic: './home.png', txt: 'Home' }, { pic: './home.png', txt: 'Home' }]
 
     // Generate the honeycomb cells
     const renderCells = () => {
@@ -25,8 +25,7 @@ function Honeycomp() {
                                 transform: `translate(${col * 160}px, ${row * 180 + (col % 2 === 0 ? 0 : 90)}px)`
                             }}
                         >
-                            <img className="image" src={images[count].pic} alt="Honeycomb Image" />
-                            <p className='text'>{images[count].txt}</p>
+                            <img className="image" src={images[count].pic} alt="Honeycomb Image" title={images[count].txt} />
                         </div>
                     );
                 }
