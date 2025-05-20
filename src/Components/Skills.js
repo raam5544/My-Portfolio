35e011392e807ed5f4bc9373da65813ab5bcb2d0
raam5544 <jeya.colombo@gmail.com>
2025-05-20 16:06:33 +0530

    Skills updated

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -3,24 +3,29 @@ import './Skills.css';
 
 function Skills() {
     const skills = [
-        { name: 'HTML', level: 'Intermediate' },
-        { name: 'CSS', level: 'Intermediate' },
-        { name: 'JAVA', level: 'Intermediate' },
-        { name: 'JavaScript', level: 'Intermediate' },
-        { name: 'React', level: 'Intermediate' },
-        { name: 'Node.js', level: 'Intermediate' },
-        { name: 'Express.js', level: 'Intermediate' },
-        { name: 'MongoDB', level: 'Intermediate' },
+        { name: 'HTML', level: 'Intermediate',symbol: 'html5'},
+        { name: 'CSS', level: 'Intermediate',symbol: 'css3' },
+        { name: 'Java', level: 'Intermediate',symbol: 'java' },
+        { name: 'Python', level: 'Intermediate',symbol: 'python' },
+        { name: 'JavaScript', level: 'Intermediate',symbol: 'javaScript' },
+        { name: 'React', level: 'Intermediate',symbol: 'react' },
+        { name: 'Node.js', level: 'Intermediate',symbol: 'nodejs' },
+        { name: 'Express.js', level: 'Intermediate',symbol: 'express' },
+        { name: 'MongoDB', level: 'Intermediate',symbol: 'mongodb' },
+        { name: 'GitHub', level: 'Intermediate',symbol: 'github' }
     ];
     return (
         <div className="skillsContainer">
-            {/* <h2>Skills</h2>  */}
+            <h2>Skills</h2> 
+            <div className="skillsGrid">
             {skills.map((skill)=>
                 <div key={skill.name} className="skill">
-                    <h3>{skill.name}</h3>
-                    <p>{skill.level}</p>
+                    {/* <h3>{skill.name}</h3> */}
+                    <img src={`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill.symbol}/${skill.symbol}-original-wordmark.svg`} alt={skill.name} />
+                    {/* <p>{skill.level}</p> */}
                 </div>
             )}
+            </div>
         </div>
     );
 }
